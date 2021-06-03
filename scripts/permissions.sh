3ebcf6add264599b723d124ff5d6e142f85bbe68
its_Saksham <38296408+sakshamk88@users.noreply.github.com>
2021-06-03 22:47:04 +0530

    file corrected

--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,4 +1,4 @@
 !#/usr/bin/env bash
-chmod -x /home/ec2-user/server/* jar
-chmod -x /home/ec2-user/server/start.sh
-chmod -x /home/ec2-user/server/stop.sh
+chmod -x /home/ec2-user/server/*.jar
+chmod -x /home/ec2-user/server/scripts/start.sh
+chmod -x /home/ec2-user/server/scripts/stop.sh
