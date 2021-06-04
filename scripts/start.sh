b9dc3ad9a46cc3feaade01331b262d231deaf44a
its_Saksham <38296408+sakshamk88@users.noreply.github.com>
2021-06-04 14:10:11 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@ cd home/ec2-user/server
 chmod 777 gradlew
 ./gradlew bootJar
 cd build/libs
-sudo java -jar demo-0.0.1-SNAPSHOT.jar >/dev/null &
+java -jar demo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
