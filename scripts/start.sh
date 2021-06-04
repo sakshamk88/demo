ff1311e55e40e310ddc7877e55e39112146bc14c
its_Saksham <38296408+sakshamk88@users.noreply.github.com>
2021-06-04 13:39:40 +0530

    update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
-kill -9 $(lsof -t -i:8080)
 cd home/ec2-user/server
 chmod 777 gradlew
 ./gradlew bootJar
 cd build/libs
-sudo java -jar demo-0.0.1-SNAPSHOT.jar
+sudo java -jar demo-0.0.1-SNAPSHOT.jar >/dev/null &
